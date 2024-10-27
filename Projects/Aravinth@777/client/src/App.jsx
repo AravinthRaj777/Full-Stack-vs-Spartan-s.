4a95a108891d5e37730d1c691ff8d8a10db8ff43
AravinthRaj777 <aravinthrajvengadesan@gmail.com>
2024-10-27 11:54:36 +0530

    Create Vite Project & Install TailwindCSS

---
+++ Projects/Aravinth@777/client/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count1, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' bg-yellow-300 text-red-400'>Hai</h1>
+    </>
+  )
+}
+
+export default App
