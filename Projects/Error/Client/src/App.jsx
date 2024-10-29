e30744078de52848549f4e3297f48b015f560d63
Logintime4 <vv0920177@gmail.com>
2024-10-29 13:47:59 +0400

    oii

--- Projects/Error/Client/src/App.jsx
+++ Projects/Error/Client/src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
    <>
    <div>
-    <span className="text-stone-500 text-6">Loginpage</span>
+    <span className="text-stone-500 text-6 items-center justify-center">Loginpage</span>
    </div>
    </>
   )
