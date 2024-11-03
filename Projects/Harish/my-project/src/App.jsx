bb4368a15ac622fa09ed0816508a9583f20d1c3d
AravinthRaj777 <aravinthrajvengadesan@gmail.com>
2024-11-03 21:14:15 +0530

    Commit

--- Projects/Harish/my-project/src/App.jsx
+++ Projects/Harish/my-project/src/App.jsx
@@ -1,20 +1,14 @@
 
 import './App.css'
 import Dark from './Page/Toggle.jsx'
-//import Toggle from './Page/Toggle'
+
 
 function App() {
   
 
   return (
     <>
-     <div className='bg-gray-500 w-screen h-screen  '>
-
-
-
-
-
-
+     <div className=' bg-slate-500 w-screen h-screen'>
       <Dark/>
     </div>
      
--- Projects/Harish/my-project/src/Page/Toggle.jsx
+++ Projects/Harish/my-project/src/Page/Toggle.jsx
@@ -1,12 +1,43 @@
-//import koil form './image/moon.jpg'
+import sun from '../assets/sun2.jpg'
+import moon from '../assets/moon2.jpg'
+
+const Toggle = () => {
+  return (
+    <>
+        <div>
+            <div className='absolute z-40 w-screen h-screen flex justify-center items-center bg-purple-600'>
+                <div className=' w-[400px] h-[200px] bg-white rounded-full flex items-center justify-end px-3 border-black border-2'> 
+                    <div className=' w-[180px] h-[180px] bg-blue-500 rounded-full border-black border-2 cursor-pointer'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+
+function handleClick(){
+    <Toggle/>
+}
 
 function Dark(){
     return (
-        <div className="flex md:justify-end px-7 py-7">
-            <div className="">
-                <img src="image/sun.jpg" className="relative h-32 w-40 top-4 rounded-3xl w-[130px] h-[60px]"/>
-                <input type="checkbox" className="bnt" id="check"/>
-                <label htmlFor="check" className="absolute h-32 w-40 top-4 rounded-3xl w-[130px] h-[60px]"></label>
+        <div className=" bg-purple-600 w-screen h-screen">
+            <div className=" relative z-0 w-screen h-screen inline-block">
+                <img src={sun} className=" absolute z-10  w-screen h-screen"/>
+                <img src={moon} className="absolute z-20  w-screen h-screen"/>
+                
+                <div>
+                    <div className='absolute z-30 w-screen h-screen flex justify-center items-center bg-purple-600'>
+                        <div className=' w-[400px] h-[200px] bg-white rounded-full flex items-center justify-start px-3 border-black border-2'> 
+                            <button onClick={handleClick} className=' w-[180px] h-[180px] bg-blue-500 rounded-full border-black border-2 cursor-pointer'>
+
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>              
     )
