a884fd04d44d26670bee1baffa89314399b9c518
Logintime4 <vv0920177@gmail.com>
2024-11-03 11:52:37 +0400

    oiiiiiiii

--- Projects/Harish/my-project/src/Page/Toggle.jsx
+++ Projects/Harish/my-project/src/Page/Toggle.jsx
@@ -2,21 +2,19 @@
 
 function Dark(){
     return (
-        <> 
-        <div className="flex md:justify-end px-3 py-7">
-            <div className="absolute h-32 w-40 top-7 bg-black rounded-3xl w-[130px] h-[60px]">
-                <input type="checkbox" id="check"/>
-                <label htmlFor="check" className=""></label>
-
-
-
-            
-                </div>
-              
+        <div className="circle">
+        <div className="flex md:justify-end px-7 py-7">
+     
+                <div className="absolute top-4 rounded-3xl w-[130px] h-[60px]">
+                <img src="image/sun.jpg" className="relative h-32 w-40 top-4 rounded-3xl w-[130px] h-[60px]"/>
+                <input type="checkbox" id="check" className="before:content-['we']"/>
+            <label htmlFor="check" className=" absolute h-32 w-40 top-4 rounded-3xl w-[130px] h-[60px]">ffffffffffffffffffffff</label>
           
-        </div>
-       
-        </>
+          
+            </div>
+            </div>
+
+            </div>      
     )
 }
 export default Dark
